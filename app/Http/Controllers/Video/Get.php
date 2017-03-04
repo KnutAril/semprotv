56838714ce68bb2daf57ca84ec01af42b27d2017
Knut Arild Evensen <knuge1@hotmail.com>
2017-03-04 17:50:13 +0100

    List videoer

--- app/Http/Controllers/Video/Get.php
+++ app/Http/Controllers/Video/Get.php
@@ -9,10 +9,31 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Get extends BaseController
 {
-    public function listVideos($numOfVideos=9)
+    public function listVideos($numOfVideos=9, $category='')
     {
-    	$videos = DB::table('video')->paginate($numOfVideos);
+    	if ($category == '')
+    	{
+    		$videos = DB::table('video')->paginate($numOfVideos);
+    	}
+    	else
+    	{
+    		$videos = DB::table('video')->where('category', '=', $category)->paginate($numOfVideos);
+    	}
 
+        return view('welcome', ['videos' => $videos]);
+    }
+
+    public function searchVideos($numOfVideos=9, $title)
+    {
+    	$videos = DB::table('video')->where('title', '=', $title)->paginate($numOfVideos);
+    	
+        return view('welcome', ['videos' => $videos]);
+    }
+
+    public function archiveVideos($numOfVideos=9, $date)
+    {
+    	$videos = DB::table('video')->where('publishdate', '=', $date)->paginate($numOfVideos);
+    	
         return view('welcome', ['videos' => $videos]);
     }
 }
